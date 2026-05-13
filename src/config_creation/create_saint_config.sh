7b5c4364eb065138ae47d6fff6d3fb83946160ef
Juan Antonio Barragan <jbarrag3@jh.edu>
2026-05-13 17:41:26 -0400

    Remove output files and change gitignore

--- src/config_creation/create_saint_config.sh
+++ src/config_creation/create_saint_config.sh
@@ -3,6 +3,6 @@
 python create_saint_config.py \
     --saint-root /home/juan95/research/discovery_grant/volumetric_drilling \
     --drill-size 4 \
-    --phantom-path /home/juan95/research/discovery_grant/saint_tools/nrrd_to_adf_hisashi/data/output2/phantom_01_05.yaml \
+    --phantom-path /home/juan95/research/discovery_grant/saint_tools/nrrd_to_adf_hisashi/data/output3/phantom_01_05.yaml \
     --marker-namespace /atracsys/drill_marker \
     --output-dir output0/
